c575e30cd491ae4a294c14d3f968f4a4d8a23c54
mahirkabir92 <mahirkabir92@gmail.com>
2023-08-24 09:42:00 -0400

    Deployment

--- js/main.js
+++ js/main.js
@@ -1,37 +1,21 @@
+ /*----- constants -----*/
+
 const wordList = ["planet", "rocket", "alien", "spaceship", "astronaut", "galaxy", "orbit", "moon", "star"];
 let chosenWord = "";
 let incorrectGuesses = 0;
 const maxAttempts = 6;
 let guessedLetters = [];
 
-function displayWord() {
-    const wordDisplay = document.getElementById("word-display");
-    let display = "";
-    for (const letter of chosenWord) {
-        if (guessedLetters.includes(letter)) {
-            display += letter + " ";
-        } else {
-            display += "_ ";
-        }
-    }
-    wordDisplay.textContent = display;
-}
 
-function checkWin() {
-    if (!document.getElementById("word-display").textContent.includes("_")) {
-        alert("Congratulations! You've guessed the word: " + chosenWord);
-        location.reload();
-    }
-}
+  /*----- state variables -----*/
 
-function checkLoss() {
-    if (incorrectGuesses >= maxAttempts) {
-        alert("Sorry, you've run out of attempts. The word was: " + chosenWord);
-        location.reload();
-    }
-}
 
-document.addEventListener("DOMContentLoaded", () => {
+  /*----- cached elements  -----*/
+
+
+  /*----- event listeners -----*/
+
+  document.addEventListener("DOMContentLoaded", () => {
     chosenWord = wordList[Math.floor(Math.random() * wordList.length)];
     displayWord();
 
@@ -54,3 +38,37 @@ document.addEventListener("DOMContentLoaded", () => {
         guessInput.value = "";
     });
 });
+
+
+  /*----- functions -----*/
+
+
+
+
+function displayWord() {
+    const wordDisplay = document.getElementById("word-display");
+    let display = "";
+    for (const letter of chosenWord) {
+        if (guessedLetters.includes(letter)) {
+            display += letter + " ";
+        } else {
+            display += "_";
+        }
+    }
+    wordDisplay.textContent = display;
+}
+
+function checkWin() {
+    if (!document.getElementById("word-display").textContent.includes("_")) {
+        alert("Congratulations!" + chosenWord);
+        location.reload();
+    }
+}
+
+function checkLoss() {
+    if (incorrectGuesses >= maxAttempts) {
+        alert("Try again! The word was: " + chosenWord);
+        location.reload();
+    }
+}
+
